ff173e2e9792ef3cd65f7095d7b54c8a4be6db4b
Future-1002 <cyntiajoanna6@gmail.com>
2024-02-12 00:03:33 -0800

    change some content

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Full Stack Developer", "JavaScript Expert"],
+    strings: ["Full Stack Developer", "Android Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
